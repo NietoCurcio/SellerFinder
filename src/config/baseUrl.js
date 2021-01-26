e5ac2d99ebd723032266c63c7f99c94a18ca70c4
Felipe Curcio <felipe_nieto010@hotmail.com>
2021-01-26 20:41:32 -0300

    Deployment

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -2,9 +2,8 @@ let baseUrl
 
 if (process.env.NODE_ENV === 'production') {
   baseUrl = process.env.BASEURL
-  console.log(baseUrl)
   console.log(process.env)
-  if (!baseUrl) baseUrl = 'https://seller-finder.herokuapp.com/'
+  if (!baseUrl) baseUrl = 'https://seller-finder.herokuapp.com'
 } else {
   baseUrl = 'http://localhost:5000'
 }
